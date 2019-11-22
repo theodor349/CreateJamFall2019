cd03bcbf8c4d8fa4954c996d0981994716dc1619
Theodor Risager <theodor349@gmail.com>
2019-11-22 21:27:43 +0100

    Folders

---
+++ CreateJamFall2019/Assets/Scripts/Earth/EarthPointer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthPointer : MonoBehaviour
+{
+    public float yHeight = 5f;
+    
+    private void Update()
+    {
+        transform.position = new Vector3();
+    }
+}
--- CreateJamFall2019/Assets/Scripts/Earth/Rotater.cs
+++ CreateJamFall2019/Assets/Scripts/Earth/Rotater.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class Rotater : MonoBehaviour
 {
     public float rotationSpeed = 2f;
-    public Transform rotationPoint;
-    
-    void FixedUpdate()
+
+    private void FixedUpdate()
     {
-        //transform.RotateAround(rotationPoint.position, Vector3.forward, rotationSpeed * Time.fixedTime);
-        transform.Rotate(Vector3.forward * rotationSpeed * Time.fixedDeltaTime, Space.World);
+        transform.Rotate(Time.fixedDeltaTime * rotationSpeed * Vector3.forward, Space.World);
     }
 }
