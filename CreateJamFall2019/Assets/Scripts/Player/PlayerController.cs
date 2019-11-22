24e28aa8f373c14b5180d47c761a5e7cb4fc4417
adamstuck <56338480+adamstuck@users.noreply.github.com>
2019-11-22 21:48:17 +0100

    Physics

--- CreateJamFall2019/Assets/Scripts/Player/PlayerController.cs
+++ CreateJamFall2019/Assets/Scripts/Player/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     private float runSpeed = 2f;
     private Rigidbody2D rbody;
 
+    [SerializeField]
+    private float gravityStrength = 9.8f;
+
     private void Awake()
     {
         rbody = GetComponent<Rigidbody2D>();
@@ -16,13 +19,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
-    }
-
-    Vector2 GetInput()
-    {
-        Vector2 inp = new Vector2();
-
-        return inp;
+        rbody.velocity += new Vector2(Input.GetAxis("Horizontal"), 0) * runSpeed * Time.deltaTime;
+        Physics.gravity = gravityStrength * (Vector3.zero - transform.position);
+        Debug.DrawRay(transform.position, Physics.gravity, Color.red);
     }
 }
