b14e9e9516d9c15a858867daa37d6fcf238fb601
Theodor Risager <theodor349@gmail.com>
2019-11-22 22:50:58 +0100

    Spawning Works

---
+++ CreateJamFall2019/Assets/Scripts/Earth/EarthProperties.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthProperties : MonoBehaviour
+{
+    public static EarthProperties Instance;
+
+    public float Radius = 2f;
+    public float RotationSpeed = 2f;
+
+    public GameObject[] SpawnableObjects;
+    
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
--- CreateJamFall2019/Assets/Scripts/Earth/EarthSpawner.cs
+++ CreateJamFall2019/Assets/Scripts/Earth/EarthSpawner.cs
@@ -1,30 +1,62 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Experimental.XR;
+using Object = UnityEngine.Object;
 
 public class EarthSpawner : MonoBehaviour
 {
-    public GameObject Prefab;
-    
+    public Transform EarthPointer;
+    public Transform Map;
+
+    private EarthProperties _earthProperties;
+
+    private void Start()
+    {
+        _earthProperties = EarthProperties.Instance;
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        {
             SpawnObject();
-        }
     }
 
     private void SpawnObject()
     {
         var spawnPoint = GetSpawnPoint();
-        Instantiate(Prefab, spawnPoint, Quaternion.identity);
+        if (spawnPoint == Vector3.zero) 
+            return;
+        var go = Instantiate(GetObjectToSpawn(), spawnPoint, Quaternion.identity);
+        go.transform.SetParent(Map);
+        go.transform.Rotate(Vector3.forward, GetRotationOfSpawn(spawnPoint));
+    }
+
+    private GameObject GetObjectToSpawn()
+    {
+        return _earthProperties.SpawnableObjects[0];
+    }
+
+    private float GetRotationOfSpawn(Vector3 point)
+    {
+        var rotation = Math.Atan(point.y / point.x) * 180/Math.PI;
+        if (point.x < 0)
+            rotation += 180;
+        
+        return (float)rotation - 90;
     }
 
     private Vector3 GetSpawnPoint()
     {
         
+        RaycastHit2D hit = Physics2D.Raycast(EarthPointer.position, Vector2.down);
+        if (hit.collider != null)
+        {
+            return hit.point;
+        }
         
         return Vector3.zero;
     }
