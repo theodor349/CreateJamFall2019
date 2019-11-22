c50e0a9d6f7ec453c51a9b7c64f5d4e4395ff2c0
Theodor Risager <theodor349@gmail.com>
2019-11-22 21:20:00 +0100

    Rotattion

---
+++ CreateJamFall2019/Assets/Scripts/Earth/Rotater.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    public float rotationSpeed = 2f;
+    public Transform rotationPoint;
+    
+    void FixedUpdate()
+    {
+        //transform.RotateAround(rotationPoint.position, Vector3.forward, rotationSpeed * Time.fixedTime);
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.fixedDeltaTime, Space.World);
+    }
+}
