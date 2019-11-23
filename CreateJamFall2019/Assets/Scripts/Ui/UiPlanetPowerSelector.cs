c4de197537405ffcc2348686798e190ac2522991
Gajhede <Hrgajhede@gmail.com>
2019-11-23 22:02:39 +0100

    some shit im doing + volcano parts

---
+++ CreateJamFall2019/Assets/Scripts/Ui/UiPlanetPowerSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UiPlanetPowerSelector : MonoBehaviour
+{
+
+    public SpriteRenderer leftPower;
+    public TextMeshProUGUI leftTimer;
+    public SpriteRenderer rightPower;
+    public TextMeshProUGUI rightTimer;
+    public SpriteRenderer currentPower;
+    public TextMeshProUGUI currentTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
