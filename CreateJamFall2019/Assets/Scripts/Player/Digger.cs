4abd5d23c3a52a54fda95376be084788f0b9959c
Theodor Risager <theodor349@gmail.com>
2019-11-23 19:41:36 +0100

    Creats Die

--- CreateJamFall2019/Assets/Scripts/Player/Digger.cs
+++ CreateJamFall2019/Assets/Scripts/Player/Digger.cs
@@ -1,10 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Digger : MonoBehaviour
 {
     public WeponSwupper weapon;
+    public Transform GunPoint;
+    public float DigDistance = 1f;
+    public LayerMask HitLayers;
     
-    
+    private void Update()
+    {
+        if (Input.GetButtonDown("P1Mine"))
+            Mine();
+    }
+
+    private void Mine()
+    {
+        var col = GetColliderInFront();
+        if(col == null)
+            return;
+
+        col.transform.GetComponent<ObjectLife>().Damage();
+    }
+
+    private Collider2D GetColliderInFront()
+    {
+        return Physics2D.Raycast(GunPoint.position, transform.right, DigDistance, HitLayers).collider;
+    }
 }
