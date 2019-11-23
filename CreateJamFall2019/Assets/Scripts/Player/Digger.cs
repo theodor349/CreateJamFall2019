e564768192438b2c8c6e85301ed8af8c23fefc3e
Theodor Risager <theodor349@gmail.com>
2019-11-23 20:14:06 +0100

    Bck On Track

--- CreateJamFall2019/Assets/Scripts/Player/Digger.cs
+++ CreateJamFall2019/Assets/Scripts/Player/Digger.cs
@@ -1,10 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
+using TMPro;
 using UnityEngine;
 
 public class Digger : MonoBehaviour
 {
     public WeponSwupper weapon;
-    
-    
+    public float DigDistance = 1f;
+    public Transform GunPoint;
+    public LayerMask HitLayer;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("P1Mine"))
+            Mine();
+    }
+
+    private void Mine()
+    {
+        var c =GetCollider();
+        if (c == null)
+            return;
+        
+        c.GetComponent<ObjectLife>().Damage();
+    }
+
+    private Collider2D GetCollider()
+    {
+        return Physics2D.Raycast(GunPoint.position, transform.right, DigDistance, HitLayer).collider;
+    }
 }
--- CreateJamFall2019/Assets/Scripts/Utillities/ObjectLife.cs
+++ CreateJamFall2019/Assets/Scripts/Utillities/ObjectLife.cs
@@ -29,6 +29,8 @@ public class ObjectLife : MonoBehaviour
     public void Damage()
     {
         stage--;
+        if (stage < 0)
+            stage = 0;
         SetSprite();
         if(stage == 0)
         {
@@ -36,6 +38,11 @@ public class ObjectLife : MonoBehaviour
             var r = gameObject.AddComponent<RotationFall>();
             r.Rotation = Random.value > 0.5 ? -90f : 90f;
             r.TimeToFall = 0.5f;
+            
+            foreach(Collider2D c in GetComponents<Collider2D>())
+            {
+                c.enabled = false;
+            }
         }
     }
 
