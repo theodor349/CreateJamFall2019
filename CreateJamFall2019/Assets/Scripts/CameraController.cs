5e4f0c70e1cfe0f88e2a2ab975e3d69795b0a720
adamstuck <56338480+adamstuck@users.noreply.github.com>
2019-11-23 14:10:01 +0100

    Started makig camera controller and added grass

---
+++ CreateJamFall2019/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float maxSize;
+    [SerializeField]
+    private float minSize;
+    [SerializeField]
+    private float fovChangeSpeed;
+
+    [SerializeField]
+    private Transform[] mainTransforms;
+
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
