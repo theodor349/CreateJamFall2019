3ebc3f06c53cf2165eef1ef1323946002cd3c4d1
adamstuck <56338480+adamstuck@users.noreply.github.com>
2019-11-23 13:39:05 +0100

    Added player sprite to prefab

---
+++ CreateJamFall2019/Assets/Scripts/Utillities/PlayerSpriteUpright.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpriteUpright : MonoBehaviour
+{
+    private Rigidbody2D rbody;
+
+
+    private void Awake()
+    {
+        rbody = transform.root.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 gravDir = Vector2.zero - rbody.position;
+        transform.up = -gravDir;
+    }
+}
