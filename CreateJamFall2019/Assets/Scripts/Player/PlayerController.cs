320b8dab9fd80bdceda76117cf1efbb0f0686662
Theodor Risager <theodor349@gmail.com>
2019-11-23 14:30:09 +0100

    new Player

--- CreateJamFall2019/Assets/Scripts/Player/PlayerController.cs
+++ CreateJamFall2019/Assets/Scripts/Player/PlayerController.cs
@@ -10,9 +10,12 @@ public class PlayerController : MonoBehaviour
     private float stopSpeed = 15f;
     [SerializeField]
     private float jumpSpeed = 8f;
+    [SerializeField]
+    SpriteRenderer spriteRenderer;
 
     private Rigidbody2D rbody;
     private bool wishJump;
+    private int collisionCount;
 
     private void Awake()
     {
@@ -21,10 +24,15 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        wishJump = Input.GetButtonDown("P1Up");
+        wishJump = Input.GetKeyDown(KeyCode.Space);
 
-        Vector2 vel = new Vector2(Input.GetAxis("P1Horizontal") * runSpeed, 0);
+        Vector2 vel = new Vector2(Input.GetAxis("Horizontal") * runSpeed, 0);
         rbody.velocity += vel * Time.deltaTime;
+
+        if (vel.x > 0)
+            spriteRenderer.flipX = true;
+        else if (vel.x < 0)
+            spriteRenderer.flipX = false;
 
         Vector2 v = rbody.velocity;
         if (rbody.velocity.x > 0 && vel.x < 0)
@@ -32,7 +40,7 @@ public class PlayerController : MonoBehaviour
         else if (rbody.velocity.x < 0 && vel.x > 0)
             v.x += stopSpeed * Time.deltaTime;
 
-        if (canJump && wishJump) {
+        if (collisionCount > 0 && wishJump) {
             wishJump = false;
             v.y = jumpSpeed;
         }
@@ -42,6 +50,11 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        canJump = true;
+        collisionCount++;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collisionCount--;
     }
 }
