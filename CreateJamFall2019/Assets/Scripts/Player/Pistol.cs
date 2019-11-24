1ac008e7e62ab77ace7ba3d105893e91df35cf61
adamstuck <56338480+adamstuck@users.noreply.github.com>
2019-11-24 05:35:45 +0100

    Camera Fix, Player 2 Water Fix

--- CreateJamFall2019/Assets/Scripts/Player/Pistol.cs
+++ CreateJamFall2019/Assets/Scripts/Player/Pistol.cs
@@ -7,12 +7,15 @@ public class Pistol : MonoBehaviour {
     [SerializeField] private GameObject waterBulletPrefab;
     [SerializeField] private float rateOfFire = 0.25f;
     [SerializeField] private float ammo = 10f;
-    [SerializeField] private RectTransform waterLevel;
+    [SerializeField] private RectTransform waterLevel1;
+    [SerializeField] private RectTransform waterLevel2;
     [SerializeField] private bool isPlayer1;
 
     private float ammoMax = 10f;
-    private float waterLevelYMin = -173.4f;
-    private float waterLevelYMax = -3.1f;
+    private float waterLevel1Min = -173.4f;
+    private float waterLevel1Max = -3.1f;
+    private float waterLevel2Min = -170f;
+    private float waterLevel2Max = -10.4f;
 
     private float nextShootTime;
     private PlayerController playerController;
@@ -83,9 +86,20 @@ public class Pistol : MonoBehaviour {
     void UpdateWaterTank()
     {
         float percent = ammo / ammoMax;
-        Vector3 pos = waterLevel.localPosition;
-        pos.y = waterLevelYMin * (1-percent);
-        pos.y = Mathf.Clamp(pos.y, waterLevelYMin, waterLevelYMax);
-        waterLevel.localPosition =  Vector3.Lerp(waterLevel.localPosition, pos, lerpSpeed * Time.deltaTime);
+        if(isPlayer1)
+        {
+            Vector3 pos = waterLevel1.localPosition;
+            pos.y = waterLevel1Min * (1 - percent);
+            pos.y = Mathf.Clamp(pos.y, waterLevel1Min, waterLevel1Max);
+            waterLevel1.localPosition = Vector3.Lerp(waterLevel1.localPosition, pos, lerpSpeed * Time.deltaTime);
+
+        }
+        else
+        {
+            Vector3 pos = waterLevel2.localPosition;
+            pos.y = waterLevel2Min * (1 - percent);
+            pos.y = Mathf.Clamp(pos.y, waterLevel2Min, waterLevel2Max);
+            waterLevel2.localPosition = Vector3.Lerp(waterLevel2.localPosition, pos, lerpSpeed * Time.deltaTime);
+        }
     }
 }
