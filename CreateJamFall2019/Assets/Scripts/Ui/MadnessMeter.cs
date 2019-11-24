575276f02b3b419ec039c13251a340258fe8cd7d
Theodor Risager <theodor349@gmail.com>
2019-11-24 02:52:42 +0100

    Madness Meter

---
+++ CreateJamFall2019/Assets/Scripts/Ui/MadnessMeter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MadnessMeter : MonoBehaviour
+{
+    public Slider Meter;
+
+    private EarthProperties _earthProperties;
+    private float lastValue;
+
+    private void Start()
+    {
+        _earthProperties = EarthProperties.Instance;
+    }
+
+    private void Update()
+    {
+        var v = (float)_earthProperties.Madness / _earthProperties.NextLevel;
+        if(Math.Abs(v - lastValue) < 0.01)
+            return;
+
+        Meter.value = v;
+        lastValue = v;
+    }
+}
