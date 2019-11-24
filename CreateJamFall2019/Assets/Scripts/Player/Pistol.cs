042a95e4883c10af2ed132ce933d84725b5370a9
adamstuck <56338480+adamstuck@users.noreply.github.com>
2019-11-24 02:54:11 +0100

    Player1 Water Tank

--- CreateJamFall2019/Assets/Scripts/Player/Pistol.cs
+++ CreateJamFall2019/Assets/Scripts/Player/Pistol.cs
@@ -6,25 +6,37 @@ public class Pistol : MonoBehaviour {
     [SerializeField] private Transform playerGraphics;
     [SerializeField] private GameObject waterBulletPrefab;
     [SerializeField] private float rateOfFire = 0.25f;
+    [SerializeField] private float ammo = 10f;
+    [SerializeField] private RectTransform waterLevel;
+
+    private float ammoMax = 10f;
+    private float waterLevelYMin = -173.4f;
+    private float waterLevelYMax = -3.1f;
 
     private float nextShootTime;
     private PlayerController playerController;
     private WeponSwupper swapper;
 
+    private float lerpSpeed = 3f;
+
+
     private void Awake() {
+        InvokeRepeating("RegainWater", 0, 1f);
         playerController = GetComponent<PlayerController>();
         swapper = GetComponent<WeponSwupper>();
     }
 
     private void Update() {
-        if(swapper.currentItem == Item.Pistol) {
-            if((Input.GetButtonDown("P1Shoot") || Input.GetAxisRaw("P1Trigger") == 1) && Time.time > nextShootTime) {
+
+        if (swapper.currentItem == Item.Pistol) {
+            if((Input.GetButtonDown("P1Shoot") || Input.GetAxisRaw("P1Trigger") == 1) && Time.time > nextShootTime && ammo > 0) {
                 Vector3 pos = transform.position;
                 if (playerController.isTurnedLeft)
                     pos += playerGraphics.right * 0.4f;
                 else
                     pos += playerGraphics.right * -0.4f;
 
+                ammo--;
                 nextShootTime = Time.time + rateOfFire;
                 GameObject bullet = Instantiate(waterBulletPrefab, pos, playerGraphics.rotation);
                 WaterBullet bulletScript = bullet.GetComponent<WaterBullet>();
@@ -43,5 +55,23 @@ public class Pistol : MonoBehaviour {
                 playerController.rbody.velocity += force * Time.deltaTime; 
             }
         }
+
+        UpdateWaterTank();
+    }
+
+    void RegainWater()
+    {
+        ammo += 1;
+        if (ammo > ammoMax)
+            ammo = ammoMax;
+    }
+
+    void UpdateWaterTank()
+    {
+        float percent = ammo / ammoMax;
+        Vector3 pos = waterLevel.localPosition;
+        pos.y = waterLevelYMin * (1-percent);
+        pos.y = Mathf.Clamp(pos.y, waterLevelYMin, waterLevelYMax);
+        waterLevel.localPosition =  Vector3.Lerp(waterLevel.localPosition, pos, lerpSpeed * Time.deltaTime);
     }
 }
