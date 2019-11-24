e6b6cf1aac400dd700d8e26713c9003f1fed6c81
Theodor Risager <theodor349@gmail.com>
2019-11-24 06:26:20 +0100

    Color

---
+++ CreateJamFall2019/Assets/Scripts/Earth/EarthColorChanger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthColorChanger : MonoBehaviour
+{
+    public SpriteRenderer Renderer;
+
+    private void Update()
+    {
+        
+    }
+}
