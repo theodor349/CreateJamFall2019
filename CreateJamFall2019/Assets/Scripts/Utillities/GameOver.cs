143979221dea79e3e85600f9932563e1a8dafbf4
adamstuck <56338480+adamstuck@users.noreply.github.com>
2019-11-24 09:04:22 +0100

    adam dead

---
+++ CreateJamFall2019/Assets/Scripts/Utillities/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] objectsToHide;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void HideObjects()
+    {
+        for(int i = 0; i < objectsToHide.Length; i++)
+        {
+            objectsToHide[i].SetActive(false);
+        }
+    }
+}
