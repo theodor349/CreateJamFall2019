8ab5d58e3786eaa23e1e46c8d07c9d4024550db1
Theodor Risager <theodor349@gmail.com>
2019-11-24 09:13:20 +0100

    Stuff

---
+++ CreateJamFall2019/Assets/Scripts/Utillities/SpawnDelay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDelay : MonoBehaviour
+{
+    public float Delay = 2f;
+    private float delayed = 0f;
+
+    public GameObject[] Spawn;
+    
+    void Update()
+    {
+        delayed += Time.deltaTime;
+        if (delayed >= Delay)
+        {
+            foreach (var g in Spawn)
+            {
+                g.SetActive(true);
+            }
+        }
+    }
+}
